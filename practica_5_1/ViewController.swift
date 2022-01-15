029e4958c68eb12b54a310b727ccdb3b7545ac21
noeromano <noeromano75@gmail.com>
2022-01-14 18:32:10 -0600

    Initial Commit

---
+++ practica_5_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practica_5_1
+//
+//  Created by Noe Romano Vazquez on 14/01/22.
+//  Copyright © 2022 noeromano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
